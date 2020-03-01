b99ac54d94d3f251e0baa4a342524315751c64a0
Alex Nagy <therebeloper@gmail.com>
2020-03-01 15:40:59 +0200

    ImagePickerView

---
+++ Sources/FlashUI/ImagePickerView.swift
@@ -0,0 +1,61 @@
+//
+//  ImagePickerView.swift
+//  
+//
+//  Created by Alex Nagy on 01/03/2020.
+//
+
+import SwiftUI
+import UIKit
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    
+    init(allowsEditing: Bool = true, delegate: UIImagePickerControllerDelegate & UINavigationControllerDelegate) {
+        self.allowsEditing = allowsEditing
+        self.delegate = delegate
+    }
+
+    private let delegate: UIImagePickerControllerDelegate & UINavigationControllerDelegate
+    private let allowsEditing: Bool
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+        let controller = UIImagePickerController()
+        controller.allowsEditing = allowsEditing
+        controller.delegate = delegate
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+    }
+}
+
+extension ImagePickerView {
+    
+    class Delegate: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        init(
+            didCancel: @escaping () -> (),
+            didSelect: @escaping (UIImage) -> ()) {
+            self.didCancel = didCancel
+            self.didSelect = didSelect
+        }
+        
+        private let didCancel: () -> ()
+        private let didSelect: (UIImage) -> ()
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            var selectedImage = UIImage()
+            if let editedImage = info[.editedImage] as? UIImage {
+                selectedImage = editedImage
+            } else if let originalImage = info[.originalImage] as? UIImage {
+                selectedImage = originalImage
+            }
+            didSelect(selectedImage)
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            didCancel()
+        }
+    }
+    
+}
